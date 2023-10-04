91be0f50e553850e8df92eafd9564e816087a46b
Harjot <hlotey@livexchange.com>
2023-10-04 13:57:47 -0700

    Added some packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
 function App() {
-  return <>
-  Coming soon ... Gig Landing Page app
-  <button>Click here to Apply</button>
-  </>;
+  return (
+    <Container>
+      <Row className="justify-content-md-center" style={{textAlign: "center"}}>
+        <Row>
+          <Col xs lg="2">
+            <h3>Gig Landing Page app</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs lg="2">
+            <Button>Click here to Apply</Button>
+          </Col>
+        </Row>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
